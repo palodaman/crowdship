62b243ea09ad7ea8df222ab39c5d4f541398626b
Isabella Tsui <126940962+Isabella-Tsui@users.noreply.github.com>
2024-10-07 21:50:24 -0700

    Fix AirGoogleMaps dir in XCode issue

--- crowdship-app/App.tsx
+++ crowdship-app/App.tsx
@@ -1,6 +1,32 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { supabase } from "./lib/supabase";
+import Auth from "./components/Auth";
+import Account from "./components/Account";
+import { View } from "react-native";
+import { Session } from "@supabase/supabase-js";
+
 import GoogleMapScreen from "./components/GoogleMapScreen";
 
-const App = () => <GoogleMapScreen />;
+export default function App() {
+  const [session, setSession] = useState<Session | null>(null);
 
-export default App;
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+  }, []);
+
+  return (
+    <View>
+      {session && session.user ? (
+        <Account key={session.user.id} session={session} />
+      ) : (
+        <Auth />
+      )}
+    </View>
+  );
+}
