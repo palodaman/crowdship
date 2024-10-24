d40fbc6e09d77523d220ea8cfa76a4e443ac69bc
Isabella Tsui <126940962+Isabella-Tsui@users.noreply.github.com>
2024-10-23 17:09:18 -0700

    Adjust the listing and loader styling

--- crowdship-app/app/deliveryrequest.tsx
+++ crowdship-app/app/deliveryrequest.tsx
@@ -1,10 +1,19 @@
 import React, { useState } from "react";
-import { View, ScrollView, Text, StyleSheet, Image, TouchableOpacity, TextInput, ActivityIndicator } from "react-native";
+import {
+  View,
+  ScrollView,
+  Text,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  TextInput,
+  ActivityIndicator,
+} from "react-native";
 import * as ImagePicker from "expo-image-picker";
 import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
 import Entypo from "@expo/vector-icons/Entypo";
 import Foundation from "@expo/vector-icons/Foundation";
-import { supabase } from '../lib/supabase';
+import { supabase } from "../lib/supabase";
 import { useSession } from "../hooks/useSession";
 
 export default function DeliveryRequest() {
@@ -19,7 +28,8 @@ export default function DeliveryRequest() {
   const session = useSession();
 
   const pickImage = async () => {
-    let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    let permissionResult =
+      await ImagePicker.requestMediaLibraryPermissionsAsync();
     if (permissionResult.granted === false) {
       alert("Permission to access camera roll is required!");
       return;
@@ -41,7 +51,8 @@ export default function DeliveryRequest() {
     setIsLoading(true);
     setMessage("");
     try {
-      const { data: userData, error: userError } = await supabase.auth.getUser();
+      const { data: userData, error: userError } =
+        await supabase.auth.getUser();
       if (userError) throw userError;
 
       const { data, error } = await supabase.from("listings").insert([
@@ -76,16 +87,28 @@ export default function DeliveryRequest() {
   };
 
   return (
-    <ScrollView contentContainerStyle={styles.scrollContainer} bounces={false} showsVerticalScrollIndicator={false}>
+    <ScrollView
+      contentContainerStyle={styles.scrollContainer}
+      bounces={false}
+      showsVerticalScrollIndicator={false}
+    >
       <View style={styles.textContainer}>
         <Text style={styles.header}>Request Delivery</Text>
 
         {/* Upload Button or Image */}
-        <View style={[styles.uploadContainer, itemImageUrl && styles.uploadedContainer]}>
+        <View
+          style={[
+            styles.uploadContainer,
+            itemImageUrl && styles.uploadedContainer,
+          ]}
+        >
           {itemImageUrl ? (
             <View style={styles.imageContainer}>
               <Image source={{ uri: itemImageUrl }} style={styles.itemImage} />
-              <TouchableOpacity style={styles.reuploadButton} onPress={pickImage}>
+              <TouchableOpacity
+                style={styles.reuploadButton}
+                onPress={pickImage}
+              >
                 <FontAwesome6 name="camera" size={18} color="white" />
                 <Text style={styles.reuploadButtonText}>Re-upload</Text>
               </TouchableOpacity>
@@ -155,7 +178,7 @@ export default function DeliveryRequest() {
           <Text style={styles.submitButtonText}>Submit Delivery Request</Text>
         </TouchableOpacity>
 
-        {isLoading && <ActivityIndicator size="large" color="#0000ff" />}
+        {isLoading && <ActivityIndicator size="large" color="#d3d3d3" />}
         {message !== "" && <Text style={styles.message}>{message}</Text>}
       </View>
     </ScrollView>
@@ -165,14 +188,14 @@ export default function DeliveryRequest() {
 const styles = StyleSheet.create({
   scrollContainer: {
     flexGrow: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     paddingTop: 15,
     paddingBottom: 100,
   },
   textContainer: {
-    alignItems: 'center',
-    width: '100%',
+    alignItems: "center",
+    width: "100%",
   },
   header: {
     fontSize: 24,
@@ -180,69 +203,69 @@ const styles = StyleSheet.create({
     marginBottom: 20,
   },
   uploadContainer: {
-    width: '75%',
+    width: "75%",
     height: 150,
-    backgroundColor: '#dde8ff',
+    backgroundColor: "#dde8ff",
     borderRadius: 10,
     padding: 15,
     marginVertical: 10,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderStyle: 'dashed',
+    justifyContent: "center",
+    alignItems: "center",
+    borderStyle: "dashed",
     borderWidth: 2,
-    borderColor: '#4a90e2',
+    borderColor: "#4a90e2",
   },
   uploadedContainer: {
-    padding: 0,  // Ensures padding remains consistent after upload
-    justifyContent: 'center', // Aligns the image in the center
-    height: 175,  // Adjusts height to fit the image better
+    padding: 0, // Ensures padding remains consistent after upload
+    justifyContent: "center", // Aligns the image in the center
+    height: 175, // Adjusts height to fit the image better
     borderWidth: 0, // Removes the border once the image is uploaded
   },
   uploadButton: {
-    backgroundColor: '#4a90e2',
+    backgroundColor: "#4a90e2",
     padding: 15,
     borderRadius: 10,
   },
   uploadButtonText: {
-    color: 'white',
-    fontWeight: 'bold',
+    color: "white",
+    fontWeight: "bold",
     fontSize: 16,
   },
   imageContainer: {
-    width: '100%',
-    height: '100%',
-    position: 'relative',
+    width: "100%",
+    height: "100%",
+    position: "relative",
   },
   itemImage: {
-    width: '100%',
-    height: '100%',
+    width: "100%",
+    height: "100%",
     borderRadius: 10,
   },
   reuploadButton: {
-    position: 'absolute',
+    position: "absolute",
     bottom: 5,
     right: 5,
-    backgroundColor: '#4a90e2',
+    backgroundColor: "#4a90e2",
     padding: 5,
     borderRadius: 10,
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
   },
   reuploadButtonText: {
-    color: 'white',
-    fontWeight: 'bold',
+    color: "white",
+    fontWeight: "bold",
     marginLeft: 5,
     fontSize: 12,
   },
   card: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    backgroundColor: 'white',
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "white",
     borderRadius: 10,
     padding: 15,
     marginVertical: 10,
-    width: '90%',
-    shadowColor: '#000',
+    width: "90%",
+    shadowColor: "#000",
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.2,
     shadowRadius: 4,
@@ -252,25 +275,25 @@ const styles = StyleSheet.create({
     flex: 1,
     marginLeft: 15,
     fontSize: 16,
-    color: 'black',
+    color: "black",
   },
   submitButton: {
     marginTop: 20,
-    backgroundColor: '#4a90e2',
+    backgroundColor: "#4a90e2",
     padding: 15,
     borderRadius: 10,
-    width: '90%',
-    alignItems: 'center',
+    width: "90%",
+    alignItems: "center",
   },
   submitButtonText: {
-    color: 'white',
-    fontWeight: 'bold',
+    color: "white",
+    fontWeight: "bold",
     fontSize: 18,
   },
   message: {
     marginTop: 20,
     fontSize: 16,
-    color: 'green',
-    textAlign: 'center',
+    color: "green",
+    textAlign: "center",
   },
 });
