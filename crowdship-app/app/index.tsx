f61ed048d680a832a82d3b40c3b09c0aa3a1e4fd
Omar Madhani <omarmadhani@w87-171-007.wireless.uvic.ca>
2024-11-17 03:22:55 -0800

    path - driver or sender

--- crowdship-app/app/index.tsx
+++ crowdship-app/app/index.tsx
@@ -1,17 +1,42 @@
 import Auth from "../components/Auth";
-import Account from "../components/Account";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, ActivityIndicator } from "react-native";
 import React from "react";
 import Header from "../components/header";
 import { useSession } from "../hooks/useSession";
+import { useState, useEffect } from 'react';
 import "./cryptoPolyfill.js";
+import Account from "../components/Account";
+import Path from "../components/Path";
+
 
 export default function App() {
-  const session = useSession();
+    const session = useSession();
+    const [currentPage, setCurrentPage] = useState<'auth' | 'path' | 'account'>('path');
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+      if (session && session.user) {
+        setLoading(false);
+      }
+    }, [session]);
+
+  const handleButtonPress = () => {
+    setCurrentPage('account');
+  };
 
   // Render the selected page based on currentPage state
   const renderPage = () => {
+    
+    if (loading) {
+      <View style={{ flex: 1, justifyContent: "center" }}>
+        <ActivityIndicator size="small" color="#d3d3d3" />
+      </View>;
+    }
+
     if (!session || !session.user) return <Auth />;
+    else if (currentPage === 'path') {
+      return <Path onButtonPress={handleButtonPress} />
+    }
     return <Account key={session.user.id} session={session} />;
   };
 
