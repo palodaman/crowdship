c586698591dc674183fd1683413ab704f7c70e72
Omar Madhani <omarmadhani@w87-170-039.wireless.uvic.ca>
2024-11-29 00:03:25 -0800

    including OR in CompleteDeliveryModal.tsx

--- crowdship-app/components/CompleteDeliveryModal.tsx
+++ crowdship-app/components/CompleteDeliveryModal.tsx
@@ -177,11 +177,14 @@ const CompleteDeliveryModal: React.FC<CompleteDeliveryModalProps> = ({
           >
             <Text style={buttonStyles.buttonText}>Upload Item Image</Text>
           </TouchableOpacity>
-          <Text style={[fontStyles.h1, { marginTop: 20 }]}>
-            In Person Delivery
+          <Text style={[fontStyles.title, { marginTop: 20 }]}>
+            OR
+          </Text>
+          <Text style={fontStyles.h1}>
+            In-Person Delivery
           </Text>
           <CheckBox
-            title="I confirm that the item was delivered in person."
+            title="I confirm that the item was delivered inperson."
             checked={check}
             onPress={handleCheck}
             checkedColor="#47BF7E"
